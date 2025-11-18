8e605ce780dd1eb099691485e4839c4583d43417
ChaeMendoza <israelmendoza656@outlook.com>
2025-11-17 23:32:31 -0500

    fix: Add margin-top to footer for proper alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 function Footer() {
     return (
-        <footer className="bg-gray-800 text-gray-200 py-4">
+        <footer className="bg-gray-800 text-gray-200 py-4 mt-auto">
             <div className="max-w-7xl mx-auto px-4 text-center">
                 <p className="text-sm">
-                    &copy; {new Date().getFullYear()} - Made with ❤️ by{" "}
-                    <span className="text-green-400 font-semibold hover:underline">
-                        Chae Mendoza
-                    </span>
+                &copy; {new Date().getFullYear()} - Made with ❤️ by{" "}
+                <span className="text-green-400 font-semibold hover:underline">
+                    Chae Mendoza
+                </span>
                 </p>
             </div>
         </footer>
