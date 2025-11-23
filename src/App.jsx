2506392e1bf81f286100060403c761a4ee2042b4
ChaeMendoza <israelmendoza656@outlook.com>
2025-11-23 07:36:28 -0500

    Fix: Protected routes and add routes to suggest

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,54 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from "./pages/index.jsx";
-import ResgisterPage from './pages/register.jsx';
+import RegisterPage from './pages/register.jsx';
 import LoginPage from './pages/login.jsx';
 import DashboardPage from './pages/dashboard.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import { useAuth } from './hooks/useAuth';
 import CalendarPage from './pages/calendar.jsx';
 import NotFound from './pages/not-found.jsx';
+import SuggestPage from "./pages/SuggestPage.jsx";
 
 function App() {
-  const { isAuthenticated } = useAuth();
+    const { isAuthenticated } = useAuth();
   
-  return (
-    <Routes>
-      <Route exact path='/' element={<Home />} />
-      <Route exact path='/register' element={<ResgisterPage />}/>
-      <Route exact path='/login' element={<LoginPage />}/>
-      <Route exact path='/calendar' element={<CalendarPage />}/>
-      <Route 
-        exact 
-        path='/dashboard' 
-        element={
-          <ProtectedRoute isAuthenticated={isAuthenticated}>
-            <DashboardPage></DashboardPage>
-          </ProtectedRoute>
-        } 
-      />
-      {/* Ruta catch-all para 404 - DEBE IR AL FINAL */}
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/register' element={<RegisterPage />}/>
+            <Route exact path='/login' element={<LoginPage />}/>
+            <Route
+                exact
+                path='/dashboard'
+                element={
+                    <ProtectedRoute isAuthenticated={isAuthenticated}>
+                        <DashboardPage></DashboardPage>
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                exact
+                path='/dashboard/suggest'
+                element={
+                    <ProtectedRoute isAuthenticated={isAuthenticated}>
+                        <SuggestPage></SuggestPage>
+                    </ProtectedRoute>
+                }
+            />
+            <Route
+                exact
+                path='/dashboard/calendar'
+                element={
+                    <ProtectedRoute isAuthenticated={isAuthenticated}>
+                        <CalendarPage></CalendarPage>
+                    </ProtectedRoute>
+                }
+            />
+            {/* Ruta catch-all para 404 - DEBE IR AL FINAL */}
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+    )
 }
 
 export default App
